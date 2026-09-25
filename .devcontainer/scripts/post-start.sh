ff60c5d60afbc5fb17fe6d9c73449a77680e504e
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2026-09-25 16:26:16 +0000

    chore: restructure

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -1,7 +1,7 @@
 # Install claude Code
-uv sync --all-groups
 
-cd src/ms_dome
+cd service/src/ms_dome/
+uv sync --all-groups
 uv run python manage.py migrate
 uv run python manage.py runserver 0.0.0.0:8000
 cd -
